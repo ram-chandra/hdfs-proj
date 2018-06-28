0712aa3c26b296083633a0810d4c70047a5f7a21
ram <ramachandra.choudki@gmail.com>
2018-06-29 04:41:26 +0530

    latest change v2

--- process-check.sh
+++ process-check.sh
@@ -2,7 +2,7 @@
 service=Namenode
 if (( $(ps -ef | grep -v grep | grep -i $service | wc -l) > 0 ))
 then
-echo "$service is running fine without no issues!!"
+echo "$service is running fine without  any issues!!"
 else
 echo "starting $service"
 /home/ram/hadoop/hadoop-2.7.4-ex1/sbin/start-dfs.sh
